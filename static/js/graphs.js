36cab09befd1a99518f9d86c3a79e233e00dc549
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-08-05 13:48:26 +0100

    add region selector

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -5,13 +5,23 @@ queue()
 function makeGraphs(error, battles) {
   var ndx = crossfilter(battles);
 
-  show_gender_balance(ndx);
+  show_region_of_battle(ndx);
+  show_battles_fought(ndx);
 
   dc.renderAll();
 }
+// show region of battle
+function show_region_of_battle(ndx) {
+  dim = ndx.dimension(dc.pluck("region"));
+  group = dim.group();
+
+  dc.selectMenu("#region_of_battle")
+    .dimension(dim)
+    .group(group);
+}
 
 // show gender balance as a bar chart
-function show_gender_balance(ndx) {
+function show_battles_fought(ndx) {
   var dim = ndx.dimension(dc.pluck("year"));
   var group = dim.group();
 
